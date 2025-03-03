5a2478c0e1ff8799030b02bc91d3459d3f7fdd7c
lucasLang0 <lucas.lang@gmail.com>
2025-03-03 15:11:05 -0800

    country

--- run_maps.sh
+++ run_maps.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-# Process only zip files from September 20th through December
-for file in /data/Twitter\ dataset/geoTwitter20-09-[2-3][0-9].zip /data/Twitter\ dataset/geoTwitter20-1[0-2]-*.zip; do
-    echo "map file: $file"
-    ./src/map.py --input_path="$file"
+for file in /data/Twitter\ dataset/geoTwitter20-*.zip; do
+	echo "map file: $file"
+	./src/map.py --input_path="$file"
 done
