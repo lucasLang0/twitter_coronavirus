f0ef497737b83b2221a7e9fa13f9e95bb87536c2
lucasLang0 <lucas.lang@gmail.com>
2025-03-03 22:58:05 -0800

    maybe

--- src/alternative_reduce.py
+++ src/alternative_reduce.py
@@ -1,61 +1,51 @@
-#!/usr/bin/env python3
-
-# command line args
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('--hashtags',nargs='+',required=True)
-#parser.add_argument('--output_path',required=True)
-args = parser.parse_args()
-
-# imports
 import os
 import json
-from collections import Counter,defaultdict
-import matplotlib
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import re
-from datetime import datetime
-import matplotlib.dates as mdates
+import sys
+from collections import defaultdict
 
-# map
-total = defaultdict(lambda: Counter())
-pattern = r'geoTwitter(20-\d{2}-\d{2})\.zip\.lang'
-for output in sorted(os.listdir("./outputs")):
-    date_file = os.path.join('./outputs', output)
-    with open(date_file) as f: 
-        date_data = json.load(f)
-        match = re.search(pattern, output)
-        if match:
-            date = match.group(1)
-            for hashtag in args.hashtags:
-                if hashtag in date_data:
-                    count = sum(date_data[hashtag].values())
-                else:
-                    count = 0
-                total[hashtag][date] += count
-#print(total)
+def scan_data_files(hashtags, data_folder='outputs'):
+    hashtag_data = defaultdict(lambda: defaultdict(int))
+    
+    for filename in os.listdir(data_folder):
+        if filename.endswith('.lang'):
+            with open(os.path.join(data_folder, filename), 'r') as file:
+                data = json.load(file)
+                date = filename.split('.')[0].split('geoTwitter')[1]
+                for hashtag in hashtags:
+                    if hashtag in data:
+                        hashtag_data[hashtag][date] += sum(data[hashtag].values())
+    
+    return hashtag_data
 
-# visualize
+def plot_data(hashtag_data, output_filename):
+    plt.figure(figsize=(10, 6))
+    
+    for hashtag, data in hashtag_data.items():
+        dates = sorted(data.keys())
+        counts = [data[date] for date in dates]
+        plt.plot(dates, counts, label=hashtag)
+    
+    plt.xlabel('Day of the Year')
+    plt.ylabel('Number of Tweets')
+    plt.title('Daily Usage of Hashtags')
+    plt.legend()
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig(output_filename)
+    plt.close()
 
-# print the count values
-hashtag_string = ''
-for hashtag in args.hashtags:
-    hashtag_string += hashtag
-    hashtag_string += '_'
-    items = sorted(total[hashtag].items(), key=lambda item: datetime.strptime(item[0], "%y-%m-%d"))
-    for k,v in items:
-        print(k,':',v)
-    tweets = [i[1] for i in items]
-    date = [datetime.strptime(i[0], "%y-%m-%d") for i in items]
-    plt.plot(date, tweets, label = hashtag)
-#date = [datetime.strptime(i[0], "%y-%m-%d") for i in total[args.hashtags[0]]]
-plt.xlabel("Date")
-plt.gca().xaxis.set_major_locator(mdates.MonthLocator(bymonthday=1))
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%y-%m-%d"))
-#plt.xticks([x for x in date if x.day == 1], rotation=45)
-plt.xticks(rotation = 45)
-plt.ylabel("Number of Tweets")
-plt.tight_layout(pad = 1.15)
-plt.legend()
-plt.savefig(f"daily_{hashtag_string}tweets_plot.png")
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python alternative_reduce.py <hashtag1> <hashtag2> ... <hashtagN>")
+        sys.exit(1)
+    
+    hashtags = sys.argv[1:]
+    hashtag_data = scan_data_files(hashtags)
+    
+    # Create a filename based on the hashtags
+    output_filename = "_".join([hashtag.strip('#') for hashtag in hashtags]) + ".png"
+    
+    plot_data(hashtag_data, output_filename)
+    
+    print(f"Plot saved as {output_filename}")
